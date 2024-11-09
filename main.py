21fa463aba9b27e91721b4faf6012b04271caaf8
siguarinc <siguarinc@unal.edu.co>
2024-11-09 18:52:33 -0500

    Prueba 2

--- main.py
+++ main.py
@@ -1,60 +1,55 @@
 class Asiento:
-    color=""
-    precio=0
-    registro=0
-    def cambiarColor(self,col):
-        if(col=="rojo"):
-            color=col
-            return
-        elif(col=="verde"):
-            color=col
-            return
-        elif(col=="Amarillo"):
-            color=col
-            return
-        elif(col=="negro"):
-            color=col
-            return
-        elif(col=="blanco"):
-            color=col
-            return
-        else:
-            return
+    def __init__(self,color,precio, registro):
+        def cambiarColor(self,col):
+            if(col=="rojo"):
+                color=col
+                return
+            elif(col=="verde"):
+                color=col
+                return
+            elif(col=="Amarillo"):
+                color=col
+                return
+            elif(col=="negro"):
+                color=col
+                return
+            elif(col=="blanco"):
+                color=col
+                return
+            else:
+                return
 
 class Motor:
-    numeroCilindro=0
-    tipo=""
-    registro=0
-    def cambiarRegistro(self):
-        return
-    def asignarTipo(self,t):
-        if(t=="electrico"):
-            tipo=t
+    def __init__(numeroCilindro,tipo,registro):
+        def cambiarRegistro(self):
             return
-        elif(t=="gasolina"):
-            tipo=t
-            return
-        return
+        def asignarTipo(self,t):
+            if(t=="electrico"):
+                tipo=t
+                return
+            elif(t=="gasolina"):
+                tipo=t
+                return
+            else:
+                return
     
 class Auto():
     cantidadCreados=""
-    def __init__():
-        modelo=""
-        precio=0
-        marca=""
-        registro=0
+    def __init__(modelo,precio,marca):
         Asiento.registro=[]
-        self.Motor=motor
-    def cantidadAsientos():
-        contta=0
-        for a in Asiento.registro:
-            if (isinstance(a,Asiento)):
-                contta+=1
-                return
-    def verificarIntegridad():
-        for a in Asiento.registro:
-            if (isinstance(a,Asiento)!=Motor.registro!=Auto.registro):
-                return"Las piezas no son originales"
-            else:
-                return"Auto original"
+        Motor.registro
+        def cantidadAsientos():
+            contta=0
+            for a in Asiento.registro:
+                if (isinstance(a,Asiento)):
+                    contta+=1
+                    return
+                else:
+                    return
+        def verificarIntegridad():
+            for a in Asiento.registro:
+                if (isinstance(a,Asiento)!=Motor.registro!=Auto.registro):
+                    return"Las piezas no son originales"
+                else:
+                    return"Auto original"
 
